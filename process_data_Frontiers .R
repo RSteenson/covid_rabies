8d1b94b8e0827bc3c843f74ed71aad5d6d145d08
Rachel S <r.steenson@hotmail.co.uk>
2021-09-07 17:25:42 +0100

    Add some basic analysis

--- process_data_Frontiers .R
+++ process_data_Frontiers .R
@@ -57,3 +57,62 @@ ggplot(data=AV_impacts_plot, aes(x=question, y=count, fill=progress)) +
   theme(legend.title = element_blank(), legend.position = "top",
         axis.text.x = element_text(angle=45, hjust=1))
 ggsave("figs/Frontiers/Animal_vaccine_impacts.pdf", height=4, width=7)
+
+#----- Analyse basic data ------------------------------------------------------
+
+# Country of research application
+country_plot = survey_data %>%
+  dplyr::select(observed.country, progress) %>%
+  group_by(progress, observed.country) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))
+country_plot$progress = factor(country_plot$progress, levels=c(1,2,3,4))
+ggplot(data=country_plot, aes(x=observed.country, y=count, fill=progress)) +
+  geom_col() +
+  scale_fill_manual(values=col_pal, labels=c("Rabies free", "Controlled", "Endemic", "Endemic with infrastructural challenges ")) +
+  labs(x="", y="Survey responses") +
+  theme_classic() +
+  theme(legend.title = element_blank(), legend.position = "top",
+        axis.text.x = element_text(angle=45, hjust=1))
+
+# Work Sector
+work_sector_responses = survey_data %>%
+  dplyr::select(observed.country, work.sector) %>%
+  group_by(work.sector) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))
+work_sector_plot = survey_data %>%
+  dplyr::select(observed.country, progress, work.sector) %>%
+  group_by(progress, work.sector) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))
+work_sector_plot$progress = factor(work_sector_plot$progress, levels=c(1,2,3,4))
+work_sector_plot$work.sector = factor(work_sector_plot$work.sector, levels=unique(work_sector_responses$work.sector))
+ggplot(data=work_sector_plot, aes(x=work.sector, y=count, fill=progress)) +
+  geom_col() +
+  scale_fill_manual(values=col_pal, labels=c("Rabies free", "Controlled", "Endemic", "Endemic with infrastructural challenges ")) +
+  labs(x="", y="Survey responses") +
+  theme_classic() +
+  theme(legend.title = element_blank(), legend.position = "top",
+        axis.text.x = element_text(angle=45, hjust=1))
+
+# Divrsion of Rabies funding
+fund_diversion_responses = survey_data %>%
+  dplyr::select(rabies.budget.reduced.diverted) %>%
+  group_by(rabies.budget.reduced.diverted) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))
+fund_diversion_plot = survey_data %>%
+  dplyr::select(observed.country, progress, rabies.budget.reduced.diverted) %>%
+  group_by(progress, rabies.budget.reduced.diverted) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))
+fund_diversion_plot$progress = factor(fund_diversion_plot$progress, levels=c(1,2,3,4))
+fund_diversion_plot$rabies.budget.reduced.diverted = factor(fund_diversion_plot$rabies.budget.reduced.diverted, levels=unique(fund_diversion_responses$rabies.budget.reduced.diverted))
+ggplot(data=fund_diversion_plot, aes(x=rabies.budget.reduced.diverted, y=count, fill=progress)) +
+  geom_col() +
+  scale_fill_manual(values=col_pal, labels=c("Rabies free", "Controlled", "Endemic", "Endemic with infrastructural challenges ")) +
+  labs(x="", y="Survey responses") +
+  theme_classic() +
+  theme(legend.title = element_blank(), legend.position = "top",
+        axis.text.x = element_text(angle=45, hjust=1))
